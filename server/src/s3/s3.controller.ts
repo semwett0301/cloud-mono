b63b06e2a532d97c1c51b159276b82faec757f64
semen.mokrov <semen.mokrov@gmail.com>
2023-12-13 08:37:12 +0300

    feat: add guard for s3

--- server/src/s3/s3.controller.ts
+++ server/src/s3/s3.controller.ts
@@ -1,19 +1,46 @@
-import { Controller, Get, NotFoundException, Param, Res } from "@nestjs/common";
+import {
+  Controller,
+  ForbiddenException,
+  Get,
+  NotFoundException,
+  Param,
+  Res,
+} from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { ProductStatus } from "@project/meta";
+import { Model } from "mongoose";
 
+import { Product } from "../scheme";
 import { S3Service } from "./s3.service";
 
 @Controller("static")
 export class S3Controller {
-  constructor(private readonly s3Service: S3Service) {}
+  constructor(
+    private readonly s3Service: S3Service,
+    @InjectModel(Product.name) private productModel: Model<Product>
+  ) {}
 
   @Get("/:id")
   async serveStaticFile(@Param("id") id, @Res() response) {
     try {
+      if (this.validateFile(id)) {
+        throw new ForbiddenException("Доступ запрещен");
+      }
+
       const s3Object = await this.s3Service.getObject(id);
+
       response.header("Content-Type", s3Object.ContentType);
       response.send(s3Object.Body);
     } catch (e) {
       throw new NotFoundException("Файл не найден");
     }
+  }
+
+  private async validateFile(fileId: string) {
+    const products = await this.productModel
+      .find({ status: ProductStatus.DISABLED })
+      .exec();
+
+    return !!products.find((product) => product.photos.includes(fileId));
   }
 }
--- server/src/s3/s3.module.ts
+++ server/src/s3/s3.module.ts
@@ -1,10 +1,20 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
 
+import { Product, ProductSchema } from "../scheme";
 import { S3Controller } from "./s3.controller";
 import { S3Service } from "./s3.service";
 
 @Module({
   controllers: [S3Controller],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Product.name,
+        schema: ProductSchema,
+      },
+    ]),
+  ],
   providers: [S3Service],
 })
 export class S3Module {}
